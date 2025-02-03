dc37fd741f5581b28e592604fa6b7b19f57f4f6e
vale.giannelli <vg.giannellivaleria@gmail.com>
2025-02-03 13:02:31 +0100

    feat: add query to list all users

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,23 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 
-namespace ConsoleApp1
+namespace EF_Config
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            //creo un'istanza della classe e specifico ilnome del DB
+            //DataContext context = new DataContext("DB_EF_Config");
+
+            //al posto del nome del DB posso mettere 'name = nameInAppConfig' e mi prende la stringa di connessione relativa 
+            DataContext context = new DataContext("name = AppConnectionString");
+
+            //usiamo context per fare la query al DB
+            //questa query mi restituisce tutti gli User nella Users Table
+            var myUsers = context.Users.ToList();
+            Console.WriteLine("Done Reading");
+            Console.Read();
         }
     }
 
+    //classe responsabile per connettere l'applicazione al DB
     class DataContext : DbContext
     {
-        //creiamo un modello che verrà usato per la comunicazione con il DB:User tra le parentesi è per specificare il tipo di dato
+        //rappresenta una collezione di oggetti di tipo User (rappresenta una tabella nel db) -> si possono fare le CRUD
         public DbSet<User> Users { get; set; }
-        
-        //costruttore
-        public DataContext()
+
+        //costruttore - posso modificare il nome. Se non lo specifichi puoi cambiarlo in appConfig: Initial Catalog=MyNewDatabase
+        public DataContext(string name):base(name)
         {
 
         }
@@ -30,7 +41,7 @@ namespace ConsoleApp1
     //creiamouna classe che definiràun'entità
     class User
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
     }
